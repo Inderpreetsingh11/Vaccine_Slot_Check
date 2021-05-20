108f3aa5afcc8464298c207d930cc8c963df6991
Inderpreetsingh11 <inderpreetsingh4632@gmail.com>
2021-05-20 14:26:27 +0530

    Update Selenium_Automation.py
    As API exposed by CoWIN is caching the data and the accurate responses can be as late as 30 mins. So made an automation script which will login into the portal(you need to fill in the OTP once) and mail you the available slots.
    PS:- This is only for the people who are struggling to book their first slot.

--- Selenium_Automation.py
+++ Selenium_Automation.py
@@ -4,37 +4,43 @@ import time
 from SendEmail import send_mail
 from pretty_html_table import build_table
 
+#Initialized an empty DataFrame to be used as buffer later
 df_buffer = pd.DataFrame()
+
 hospital_names = []
 Availability = []
 Vaccine = []
 Age = []
 Dates = []
+
+#chromedriver to be used with selenium
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome('Enter the path where you have downloaded the chromedriver')
+#CoWIN's website to reach
 driver.get('https://selfregistration.cowin.gov.in/')
 time.sleep(3)
 input = driver.find_element_by_id('mat-input-0')
-input.send_keys('Enter you rphone number here')
+#Enter your registered phone number
+input.send_keys('Enter you phone number here')
 get_otp_button = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col[1]/ion-grid/form/ion-row/ion-col[2]/div/ion-button")
 get_otp_button.click()
 #Enter the OTP that you have recieved on your mobile here and wait for the verify proceed button to be clicked automatically
 time.sleep(20)
 verify_proceed_button = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col/ion-grid/form/ion-row/ion-col[3]/div/ion-button")
 verify_proceed_button.click()
-time.sleep(3)
+time.sleep(2)
 schedule_button = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-beneficiary-dashboard/ion-content/div/div/ion-grid/ion-row/ion-col/ion-grid[1]/ion-row[2]/ion-col/ion-grid/ion-row[4]/ion-col[2]/ul/li/a")
 
 schedule_button.click()
-time.sleep(3)
+time.sleep(2)
 enter_pin = driver.find_element_by_xpath('//*[@id="mat-input-2"]')
+#Enter the puncode for which you want to search
 enter_pin.send_keys('Enter your pincode here')
 while True:
     search_button = driver.find_element_by_xpath ("/html/body/app-root/ion-app/ion-router-outlet/app-appointment-table/ion-content/div/div/ion-grid/ion-row/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col[2]/form/ion-grid/ion-row/ion-col[4]/ion-button")
     search_button.click()
     time.sleep(3)
-    #data = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-appointment-table/ion-content/div/div/ion-grid/ion-row/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col[2]/form/ion-grid/ion-row/ion-col[9]/div/div/ul/carousel/div/div")
-    for i in range(1,50):
+     for i in range(1,50):
         try:
             data = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-appointment-table/ion-content/div/div/ion-grid/ion-row/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col[2]/form/ion-grid/ion-row/ion-col[10]/div/div/mat-selection-list/div[{}]/mat-list-option/div/div[2]/ion-row/ion-col[1]/div/h5".format(i))
             name_of_hospital = data.get_attribute('textContent')
@@ -69,7 +75,7 @@ while True:
                    'Vaccine' : Vaccine,
                    'Age' : Age
                    }
-    #print(len(hospital_names),len(Vaccine),len(Dates),len(Availability),len(Age))
+
     df = pd.DataFrame(data_for_df)
     if df_buffer.equals(df):
         pass
@@ -77,6 +83,7 @@ while True:
         df_buffer = df
         output = build_table (df, 'blue_light')
         send_mail (output)
+
     print(df)
     print('executed')
     hospital_names.clear()
